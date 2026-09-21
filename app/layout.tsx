d7de1391351b28d5e252b94717326c3c0bb5912e
sniffnsnooz <sniffnsnooz@gmail.com>
2026-09-21 14:41:22 -0700

    chore: add Google Tag Manager (GTM-WRS3SCPB) as requested

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,6 +97,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* ========================= 
+            📊 GOOGLE TAG MANAGER (GTM-WRS3SCPB)
+        ========================= */}
+        <Script
+          id="gtm-init"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WRS3SCPB');
+            `,
+          }}
+        />
+
         {/* ✅ Google Search Console Verification */}
         <meta name="google-site-verification" content="rBLyCvgHVJgwOvIgpBPXEs1N4boYVbewvVqWiejhDMo" />
 
@@ -130,33 +147,18 @@ export default function RootLayout({
             `,
           }}
         />
-
-        {/* ========================= 
-            📊 GOOGLE TAG MANAGER (GT-K4TLPZHK)
-        ========================= */}
-        <Script
-          id="gtm-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GT-K4TLPZHK');
-            `,
-          }}
-        />
       </head>
-      <body>
-        {/* ✅ GTM noscript fallback (required by Google) */}
+      <body className="antialiased min-h-screen flex flex-col bg-stone-50 font-sans selection:bg-amber-200">
+        {/* ========================= 
+            📊 GOOGLE TAG MANAGER (noscript)
+        ========================= */}
         <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GT-K4TLPZHK"
-            height="0"
-            width="0"
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WRS3SCPB"
+            height="0" 
+            width="0" 
             style={{ display: "none", visibility: "hidden" }}
-          />
+          ></iframe>
         </noscript>
 
         <AppPreloader />
